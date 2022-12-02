01d66440fadae959080ed85456f84a3bdf10c040
takumatt <takuma.matt@gmail.com>
2022-12-02 16:54:58 +0900

    Refer to arguments instead of env

--- preview-generator.sh
+++ preview-generator.sh
@@ -1,5 +1,17 @@
 source .env
 
+echo $1
+echo $2
+echo $#
+
+if [[ $# -ne 2 ]]; then
+    echo "Usage: $CMDNAME [UDID] [target-runtime]"
+    exit 1
+fi
+
+UDID=$1
+TARGET_RUNTIME=$2
+
 swift PreviewProviderExtractor.swift DemoView.swift | xargs swift PreviewCodeBuilder.swift > ./app/main.swift
 cp DemoView.swift ./app
 sh -x $SCRIPT_DIR/run-on-simulator.sh $UDID $TARGET_RUNTIME
